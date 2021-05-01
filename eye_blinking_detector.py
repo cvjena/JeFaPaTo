8aad60f072b663e27b554a1839e8e557e53b5555
tim.buechner <tim.buechner@uni-jena.de>
2021-05-01 16:49:19 +0200

    Improve face detection speed by downscaling the image

--- eye_blinking_detector.py
+++ eye_blinking_detector.py
@@ -10,6 +10,40 @@ import glob
 import imutils
 from imutils import face_utils
 
+def scale_bbox(bbox: dlib.rectangle, scale: float, padding: int = 0) -> dlib.rectangle:
+    """Scale and pad a dlib.rectangle
+
+    Copyright: Tim Büchner tim.buechner@uni-jena.de
+
+    This function will scale a dlib.rectangle and can also apply additional padding
+    to the rectangle. If a padding value of -1 is given, the padding is will be
+    a fourth of the respective widht and height.
+
+    Args:
+        bbox (dlib.rectangle): rectangle which will be scaled and optionally padded
+        scale (float): scaling factor for the rectangle
+        padding (int, optional): Additional padding for the rectangle. Defaults to 0. If -1 use fourth of width and height for padding.
+
+    Returns:
+        dlib.rectangle: newly scaled (and optionally padded) dlib.rectangle
+    """    
+    left: int = int(bbox.left() * scale)
+    top: int = int(bbox.top() * scale)
+    right: int = int(bbox.right() * scale)
+    bottom: int = int(bbox.bottom() * scale)
+
+    width = right - left
+    height = bottom - top
+
+    padding_w = width // 4 if padding == -1 else padding
+    padding_h = height // 4 if padding == -1 else padding
+
+    return dlib.rectangle(
+        left=left - padding_w,
+        top=top - padding_h,
+        right=right + padding_w,
+        bottom=bottom + padding_h,
+    )
 
 class EyeBlinkingDetector():
     def __init__(self, threshold):
@@ -29,17 +63,21 @@ class EyeBlinkingDetector():
         self.left_eye_closing_norm_area = -1
         self.right_eye_closing_norm_area = -1
 
+        self.scale_factor = 0.3
+
     def set_threshold(self, threshold):
         self.threshold = threshold
 
     def detect_eye_blinking_in_image(self, image: np.ndarray, eye_info: Mapping):
         # image = imutils.resize(image, width=500)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_sc = cv2.resize(gray, (int(image.shape[1] * self.scale_factor), int(image.shape[0] * self.scale_factor)), interpolation=cv2.INTER_LINEAR)
         # detect faces in the grayscale image
-        rects = self.detector(gray, 1)
+        rects = self.detector(gray_sc, 1)
 
         # loop over the face detections
-        for (i, rect) in enumerate(rects):
+        for i, rect in enumerate(rects):
+            rect = scale_bbox(rect, 1/self.scale_factor)
             # determine the facial landmarks for the face region, then
             # convert the facial landmark (x, y)-coordinates to a NumPy
             # array
