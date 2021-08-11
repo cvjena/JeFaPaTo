ddea1248da4f2ae38cb5b7e06d783450f1eeaf57
Tim Büchner <tim.buechner@uni-jena.de>
2021-08-11 14:12:42 +0200

    Fix typo

--- export.sh
+++ export.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -r jefapato.zip config/ data/ jefapato/ ui/ landmark_distances.py LICENSE.txt main.py README.md view_emotion_recognition.py view_eye_blinking.py view_landmark_distances_2D.py view_landmark_distances_3D.py start_jefapato.bat env.yml create_jefapato.bat update_jefapto.bat
+zip -r jefapato.zip config/ data/ jefapato/ ui/ landmark_distances.py LICENSE.txt main.py README.md view_emotion_recognition.py view_eye_blinking.py view_landmark_distances_2D.py view_landmark_distances_3D.py start_jefapato.bat env.yml create_jefapato.bat update_jefapato.bat
