db331761a575544314c1b4913553c036135a6d59
Tim Büchner <tim.buechner@uni-jena.de>
2021-10-25 08:58:25 +0200

    Update export script

--- export.sh
+++ export.sh
@@ -5,14 +5,11 @@ config/ \
 data/ \
 jefapato/ \
 ui/ \
+frontend/ \
 landmark_distances.py \
 LICENSE.txt \
 main.py \
 README.md \
-view_emotion_recognition.py \
-view_eye_blinking.py \
-view_landmark_distances_2D.py \
-view_landmark_distances_3D.py \
 start_jefapato.bat \
 env.yml \
 create_jefapato.bat \
