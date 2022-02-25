a0e2fdbe75c4f58533866a1232c4171706f24a5a
Tim Büchner <tim.buechner@uni-jena.de>
2022-02-25 11:03:28 +0100

    Added logging to export script

--- export.py
+++ export.py
@@ -1,6 +1,10 @@
 import pathlib
 import zipfile
 
+import structlog
+
+logger = structlog.get_logger()
+
 
 def write_dir_to_zip(zfile: zipfile.ZipFile, dir_path: pathlib.Path) -> None:
     """
@@ -9,8 +13,11 @@ def write_dir_to_zip(zfile: zipfile.ZipFile, dir_path: pathlib.Path) -> None:
     :param zfile: Zip file to write to.
     :param dir_path: Path to directory to write.
     """
+    logger.info("Writing directory to zip file", dir_path=dir_path)
+
     for child in dir_path.iterdir():
         if child.is_file():
+            logger.info("Writing file to zip file", file_path=child)
             zfile.write(child.as_posix())
         elif child.is_dir():
             if child.name == "__pycache__":
@@ -25,6 +32,16 @@ export_dir = pathlib.Path("__exports__")
 export_dir.mkdir(exist_ok=True, parents=True)
 file_dir = export_dir / f"{file_name}.zip"
 jefapato_z = zipfile.ZipFile(file_dir, "w")
+system = "Windows"
+
+logger.info(
+    "Start export of JeFaPaTo",
+    version=version,
+    file_name=file_name,
+    system=system,
+    file_dir=file_dir,
+)
+logger.info("Writing files to zip file", file_name=file_name)
 
 # add assets
 write_dir_to_zip(jefapato_z, pathlib.Path("assets"))
@@ -37,17 +54,23 @@ write_dir_to_zip(jefapato_z, pathlib.Path("ui"))
 
 # add environment
 jefapato_z.write("env.yml")
+logger.info("Writing environment to zip file", file_name=file_name)
 
 # add main file
 jefapato_z.write("main.py")
+logger.info("Write main file to zip file", file_name=file_name)
 
 # add bash scripts
 jefapato_z.write("jefapato_create.bat")
 jefapato_z.write("jefapato_start.bat")
 jefapato_z.write("jefapato_update.bat")
+logger.info("Write bash scripts to zip file", file_name=file_name)
 
 # add README and LICENSE
 jefapato_z.write("LICENSE.txt")
 jefapato_z.write("README.md")
+logger.info("Write README and LICENSE to zip file", file_name=file_name)
 
 jefapato_z.close()
+logger.info("Closed zip file", file_name=file_name)
+logger.info("Export complete", file_name=file_name)
