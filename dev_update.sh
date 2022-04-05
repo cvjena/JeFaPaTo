24a2c21919b834b559f6f585a0350a27811fb4a7
Tim Büchner <tim.buechner@uni-jena.de>
2022-04-05 09:10:03 +0200

    Update the requirements for Jefapato

--- dev_update.sh
+++ dev_update.sh
@@ -6,3 +6,7 @@ python -m pip install -r requirements.txt
 
 pre-commit install
 pre-commit autoupdate
+
+curl -O https://google.github.io/styleguide/pylintrc
+sed -i "s/indent-string='  '/indent-string='    '/" pylintrc
+sed -i "s/max-line-length=80/max-line-length=88/" pylintrc
