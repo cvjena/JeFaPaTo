7bf63265ca8637769d3401401a88b3f52444f02d
Tim Büchner <tim.buechner@uni-jena.de>
2023-07-19 09:46:52 +0200

    small fix

--- frontend/start_up/start_up_application_attributes.py
+++ frontend/start_up/start_up_application_attributes.py
@@ -1,16 +1,14 @@
-from qtpy import QtCore, QtWidgets
+from PyQt6.QtWidgets import QSplashScreen, QApplication
+from PyQt6.QtCore import Qt
 
 from .enum_state import StartUpState
 
 
-def start_up_appliciation_attributes(
-    splash_screen: QtWidgets.QSplashScreen, **kwargs
-) -> StartUpState:
+def start_up_appliciation_attributes(splash_screen: QSplashScreen, app: QApplication, **kwargs) -> StartUpState:
     splash_screen.showMessage("Setting application attributes...")
 
-    app = kwargs.get("app")
-    if hasattr(QtCore.Qt, "AA_UseHighDpiPixmaps"):
-        app.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps)
+    if hasattr(Qt.ApplicationAttribute, "AA_UseHighDpiPixmaps"):
+        app.setAttribute(Qt.ApplicationAttribute.AA_UseHighDpiPixmaps)
 
     splash_screen.showMessage("Application attributes set...")
     return StartUpState.SUCCESS
