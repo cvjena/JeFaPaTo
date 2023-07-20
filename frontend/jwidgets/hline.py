6b787a5d054f9fefe08c8fa4bf029921407ab4f9
Tim Büchner <tim.buechner@uni-jena.de>
2023-07-20 09:36:57 +0200

    fix missing properties

--- frontend/jwidgets/hline.py
+++ frontend/jwidgets/hline.py
@@ -5,5 +5,5 @@ from PyQt6.QtWidgets import QFrame
 class JHLine(QFrame):
     def __init__(self):
         super().__init__()
-        self.setFrameShape(QFrame.HLine)
-        self.setFrameShadow(QFrame.Sunken)
+        self.setFrameShape(QFrame.Shape.HLine)
+        self.setFrameShadow(QFrame.Shadow.Sunken)
--- frontend/jwidgets/vline.py
+++ frontend/jwidgets/vline.py
@@ -5,5 +5,5 @@ from PyQt6.QtWidgets import QFrame
 class JVLine(QFrame):
     def __init__(self):
         super().__init__()
-        self.setFrameShape(QFrame.VLine)
-        self.setFrameShadow(QFrame.Sunken)
+        self.setFrameShape(QFrame.Shape.VLine)
+        self.setFrameShadow(QFrame.Shadow.Sunken)
