9ec8ca402ff840201fcb5c75eb573c7d39302a22
Tim Büchner <tim.buechner@uni-jena.de>
2023-07-20 10:52:57 +0200

    set table properties

--- frontend/jwidgets/blinkingtable.py
+++ frontend/jwidgets/blinkingtable.py
@@ -12,26 +12,43 @@ def to_qt_row(row: pd.Series) -> list:
 class JBlinkingTable(QSplitter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.setOrientation(Qt.Orientation.Horizontal)
 
         self.table_left_eye = QTableView()
         self.table_right_eye = QTableView()
+        self.addWidget(self.table_left_eye)
+        self.addWidget(self.table_right_eye)
+
+        # set the model such that the whole list is selected when a row is selected
+        self.table_left_eye.setSelectionBehavior(QTableView.SelectionBehavior.SelectRows)
+        self.table_right_eye.setSelectionBehavior(QTableView.SelectionBehavior.SelectRows)
+        # set the model such that only one row can be selected at a time
+        self.table_left_eye.setSelectionMode(QTableView.SelectionMode.SingleSelection)
+        self.table_right_eye.setSelectionMode(QTableView.SelectionMode.SingleSelection)
+        # set the model such that the user cannot edit the table
+        self.table_left_eye.setEditTriggers(QTableView.EditTrigger.NoEditTriggers)
+        self.table_right_eye.setEditTriggers(QTableView.EditTrigger.NoEditTriggers)
+        # set the model such that the user cannot move the columns
+        self.table_left_eye.horizontalHeader().setSectionsMovable(False)
+        self.table_right_eye.horizontalHeader().setSectionsMovable(False)
 
         self.model_left_eye = QStandardItemModel()
         self.model_right_eye = QStandardItemModel()
 
         self.table_left_eye.setModel(self.model_left_eye)
         self.table_right_eye.setModel(self.model_right_eye)
-
-        self.addWidget(self.table_left_eye)
-        self.addWidget(self.table_right_eye)
-
+        
+        # if the user clicks on a row, the other table should also select the same row
+        self.table_left_eye.selectionModel().selectionChanged.connect(
+            lambda selected, deselected: self.table_right_eye.selectRow(selected.indexes()[0].row())
+        )
+        self.table_right_eye.selectionModel().selectionChanged.connect(
+            lambda selected, deselected: self.table_left_eye.selectRow(selected.indexes()[0].row())
+        )
 
     def reset(self):
         self.model_left_eye.clear()
         self.model_right_eye.clear()
-
 
     def set_data(self, data_left: pd.DataFrame, data_right: pd.DataFrame):
         assert data_left is not None and isinstance(data_left, pd.DataFrame)
