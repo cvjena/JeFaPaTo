3345007ea5718dce30fe4e1772f8000a986ff783
Tim Büchner <tim.buechner@uni-jena.de>
2023-07-26 09:20:38 +0200

    add nuitka for compilation

--- dev_init.sh
+++ dev_init.sh
@@ -5,7 +5,7 @@ eval "$(conda shell.bash hook)"
 conda create -n jefapato -y
 conda activate jefapato
 conda install python=3.10 pip -y
-
+conda install libpython-static
 python --version
 
 # use pip as a module in the install python. this way we can avoid using the
