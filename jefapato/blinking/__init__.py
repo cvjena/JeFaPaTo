f80d723ac8e1ccac6b78eb8fb464e133c0cc7e58
Tim Büchner <tim.buechner@uni-jena.de>
2023-11-13 10:42:17 +0100

    Add skeleton for new summary function

--- jefapato/blinking/__init__.py
+++ jefapato/blinking/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["peaks", "smooth", "match"]
+__all__ = ["peaks", "smooth", "match", "summarize"]
 
 from jefapato.blinking.peaks import peaks
 from jefapato.blinking.smooth import smooth
 from jefapato.blinking.match import match
+from jefapato.blinking.summary import summarize
---
+++ jefapato/blinking/summary.py
@@ -0,0 +1,9 @@
+__all__ = ["summarize"]
+
+import pandas as pd
+
+def summarize(
+    merged_blinking: pd.DataFrame,
+    fps: int = 240,
+) -> pd.DataFrame:
+    pass
