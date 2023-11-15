e37bffdc3a809aadf66fa1f61f9ab781d97645e1
Tim Büchner <tim.buechner@uni-jena.de>
2023-11-15 14:11:25 +0100

    Setup environment to force old mediapipe version

--- dev_init.sh
+++ dev_init.sh
@@ -2,10 +2,9 @@
 
 # https://stackoverflow.com/questions/34534513/calling-conda-source-activate-from-bash-script
 eval "$(conda shell.bash hook)"
-conda create -n jefapato -y
-conda activate jefapato
+conda create -n jefapato-intel-mediapipe-v9 -y
+conda activate jefapato-intel-mediapipe-v9
 conda install python=3.10 pip -y
-conda install libpython-static -y
 python --version
 
 # use pip as a module in the install python. this way we can avoid using the
--- dev_update.sh
+++ dev_update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
-conda activate jefapato
+conda activate jefapato-intel-mediapipe-v9
 python -m pip install -r requirements-dev.txt
 python -m pip install -r requirements.txt
 
-pre-commit install
-pre-commit autoupdate
+# pre-commit install
+# pre-commit autoupdate
