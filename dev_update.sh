90d9a3bf10540cde03d5c13fdf3e225d61251d20
Tim Büchner <tim.buechner@uni-jena.de>
2023-11-20 16:30:37 +0100

    fix dev_init and remove dev_update

--- dev_update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-eval "$(conda shell.bash hook)"
-conda activate jefapato
-python -m pip install -r requirements-dev.txt
-python -m pip install -r requirements.txt
-
-pre-commit install
-pre-commit autoupdate
