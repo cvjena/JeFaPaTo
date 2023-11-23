1d944f2b14355832d7ac77406baedb9a812cc89f
Tim Büchner <tim.buechner@uni-jena.de>
2023-11-23 11:28:21 +0100

    Rename file for JVideoFaceSelection widget

--- frontend/jwidgets/__init__.py
+++ frontend/jwidgets/__init__.py
@@ -4,7 +4,7 @@ from frontend.jwidgets.blinkingtable import JBlinkingTable
 from frontend.jwidgets.collapsiblebox import JCollapsibleBox
 from frontend.jwidgets.graph import JGraph
 from frontend.jwidgets.hline import JHLine
-from frontend.jwidgets.image_box import JVideoFaceSelection
+from frontend.jwidgets.video_face_selection import JVideoFaceSelection
 from frontend.jwidgets.imagebox import JImageBox
 from frontend.jwidgets.videofacepreview import JVideoFacePreview
 from frontend.jwidgets.vline import JVLine
