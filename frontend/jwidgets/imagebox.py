7ca7bbcd213f92e82d5880c76b5a7969648c76dd
Tim Büchner <tim.buechner@uni-jena.de>
2025-03-26 09:10:03 +0100

    Utilize Mediapipe also for the preview

--- frontend/jwidgets/imagebox.py
+++ frontend/jwidgets/imagebox.py
@@ -2,13 +2,38 @@ __all__ = ["JImageBox"]
 
 import numpy as np
 import pyqtgraph as pg
+from PyQt6.QtGui import QCursor
+from PyQt6 import QtCore
+
 
 class JImageBox(pg.ViewBox):
-    def __init__(self, **kwargs):
+    def __init__(self, interactive: bool = False, **kwargs):
         super().__init__(invertY=True, lockAspect=True, **kwargs)
         self.frame = pg.ImageItem()
+        self.interactive = interactive
         self.addItem(self.frame)
         self.setBackgroundColor("w")
 
+        # if interactive changet the cursor to a cross
+        if self.interactive:
+            # self.setMouseMode(self.RectMode)
+            self.setCursor(QCursor(QtCore.Qt.CursorShape.OpenHandCursor))
+
+        self._bbox_rect = None
+
     def set_image(self, image: np.ndarray) -> None:
         self.frame.setImage(image)
+        self.setLimits(xMin=0, xMax=image.shape[1], yMin=0, yMax=image.shape[0])
+        self.setRange(xRange=[0, image.shape[1]], yRange=[0, image.shape[0]], padding=0)
+
+    def set_image_with_bbox(self, image: np.ndarray, bbox: tuple[int, int, int, int]) -> None:
+        self.set_image(image)
+
+        x1, y1, x2, y2 = bbox
+        if self._bbox_rect:
+            self.removeItem(self._bbox_rect)
+        self._bbox_rect = pg.RectROI([x1, y1], [x2 - x1, y2 - y1], pen=pg.mkPen("r", width=4), movable=False)
+        self.addItem(self._bbox_rect)
+
+        # zoom to bbox, but autorange is not working
+        self.setRange(xRange=[x1, x2], yRange=[y1, y2], padding=0)
--- frontend/jwidgets/videofacepreview.py
+++ frontend/jwidgets/videofacepreview.py
@@ -11,6 +11,7 @@ from PyQt6.QtWidgets import QLabel, QVBoxLayout, QWidget
 from structlog import get_logger
 
 from frontend.jwidgets.imagebox import JImageBox
+from jefapato.facial_features import MediapipeFaceModel
 
 logger = get_logger()
 
@@ -21,10 +22,9 @@ class FaceVideoContainer:
         self.resource: cv2.VideoCapture | None = None  # TODO decord as alternative useful?
         self.frame_count: int | None = None
 
-        ccc_file = Path(__file__).parent / "models" / "haarcascade_frontalface_default.xml"
-        self.face_finder = cv2.CascadeClassifier(str(ccc_file))
+        self.model = MediapipeFaceModel()
 
-    def load_file(self, file_path: Path) -> np.ndarray:
+    def load_file(self, file_path: Path) -> tuple[np.ndarray, tuple[int, int, int, int]]:
         assert file_path.exists()
         assert file_path.is_file()
 
@@ -37,34 +37,29 @@ class FaceVideoContainer:
         assert self.frame_count is not None
         return frame_index >= 0 and frame_index < self.frame_count
 
-    def get_frame(self, frame_index: int) -> np.ndarray:
+    def get_frame(self, frame_index: int) -> tuple[np.ndarray, tuple[int, int, int, int]]:
+        """
+        Returns the frame at the given index and the face bounding box.
+        """
+
         assert self.resource is not None
 
+        # TODO here might be a strong offset to the actual frame index if the video is very very long
         self.resource.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
         ret, frame = self.resource.read()
         if not ret:
             logger.error("Could not read frame", frame_index=frame_index, file_path=self.file_path)
-            return np.zeros((300, 300, 3), dtype=np.uint8)
+            return np.zeros((300, 300, 3), dtype=np.uint8), (0, 0, 300, 300)
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_g = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        faces = self.face_finder.detectMultiScale(frame_g, 1.1, 5)
-        if len(faces) == 0:
+        landmarks, _, valid = self.model.extract(frame)
+
+        if not valid:
             logger.warning("Could not find face", frame_index=frame_index, file_path=self.file_path)
-            return frame
+            return frame, (0, 0, frame.shape[1], frame.shape[0])
 
-        x, y, w, h = faces[0]
-        # # scale 0.25 in the y direction
-        # y = max(0, y - int(h * 0.25))
-        # h = int(h * 1.5)
-
-        x = 0 if x < 0 else x
-        y = 0 if y < 0 else y
-        w = frame.shape[1] - x if x + w > frame.shape[1] else w
-        h = frame.shape[0] - y if y + h > frame.shape[0] else h
-
-        frame = frame[y : y + h, x : x + w]
-        return frame
+        bbox = self.model.lmk_to_bbox(landmarks)
+        return frame, bbox
 
     def is_loaded(self) -> bool:
         return self.resource is not None
@@ -102,7 +97,7 @@ class JVideoFacePreview(QWidget):
         self.vbox.addWidget(self.text_dragdrop)
 
         self.glayout = pg.GraphicsLayoutWidget()
-        self.face_widget = JImageBox(enableMouse=False, enableMenu=False)
+        self.face_widget = JImageBox(interactive=True, enableMouse=True, enableMenu=False)
         self.glayout.addItem(self.face_widget)
 
         self.face_container = FaceVideoContainer()
@@ -124,8 +119,8 @@ class JVideoFacePreview(QWidget):
             self.vbox.addWidget(self.glayout)
 
         # then load the file
-        frame = self.face_container.load_file(file_path)
-        self.face_widget.set_image(frame)
+        frame, bbox = self.face_container.load_file(file_path)
+        self.face_widget.set_image_with_bbox(frame, bbox)
 
     def set_frame(self, frame_idx: int) -> None:
         assert self.face_container is not None
@@ -139,8 +134,8 @@ class JVideoFacePreview(QWidget):
             logger.error("Invalid frame index", frame_idx=frame_idx, frame_count=self.face_container.frame_count)
             return
 
-        frame = self.face_container.get_frame(frame_idx)
-        self.face_widget.set_image(frame)
+        frame, bbox = self.face_container.get_frame(frame_idx)
+        self.face_widget.set_image_with_bbox(frame, bbox)
 
     def dragEnterEvent(self, event: QtGui.QDropEvent):  # type: ignore
         logger.info("User started dragging event", widget=self)
